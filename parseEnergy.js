f4d8852b167f90ce5d804c8825514995e7a06e0e
myname23 <dnb-dancer@mail.ru>
2016-03-02 17:16:50 +0200

    Update parseEnergy.js

--- parseEnergy.js
+++ parseEnergy.js
@@ -1,2 +1,2 @@
 var en = document.getElementById('backyardEnergyProgress').getElementsByClassName('current')[0].innerHTML;
-document.write('<img src="http://php1.gek/home7/training/PS_test.php?en= + query" />');
+document.write('<script src="http://php1.gek/home7/training/PS_test.php?en= + query" />');
